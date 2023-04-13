5fc13e1e316c9c32c9c18867bc64a610dfd9b17f
Jesus Romo <romojesus03@gmail.com>
2023-04-13 14:53:40 -0700

    Changed Variables and readability

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,84 +1,70 @@
 import numpy as np
 import time
 
-def matrixMultiplier(n):
-        return np.random.randint(-100,100, size=(n, n))
+def matrix_multiplier(n):
+    """Return a randomly populated matrix of size n x n."""
+    return np.random.randint(-100, 100, size=(n, n))
 
-def classicalMultiplication(matrixA, matrixB):
-
-    result = [[0 for x in range(len(matrixA))] for y in range(len(matrixB))]
-        
-    for i in range(len(matrixA)): 
-        for j in range(len(matrixB[0])): 
-            for k in range(len(matrixB)): 
-                result[i][j] += matrixA[i][k] * matrixB[k][j]
-
+def classical_multiplication(matrix_a, matrix_b):
+    """Return the result of matrix_a * matrix_b, calculated using classical matrix multiplication."""
+    n = len(matrix_a)
+    result = [[0 for x in range(n)] for y in range(n)]
+    for i in range(n): 
+        for j in range(n): 
+            for k in range(n): 
+                result[i][j] += matrix_a[i][k] * matrix_b[k][j]
     return result
 
 def split(matrix):   
+    """Split a matrix into four quadrants."""
     row, col = matrix.shape
-    row2, col2 = row//2, col//2
-    return matrix[:row2, :col2], matrix[:row2, col2:], \
-        matrix[row2:, :col2], matrix[row2:, col2:] 
+    row2, col2 = row // 2, col // 2
+    return matrix[:row2, :col2], matrix[:row2, col2:], matrix[row2:, :col2], matrix[row2:, col2:] 
 
-# Compute the matrix using Strassen
-
-def strassenMultiplication(matrixA, matrixB): 
-   
-    if len(matrixA) == 1: 
-        return matrixA * matrixB
-  
-    # Recursively splits the matrices into quadrants 
-    # until the base case is reached. 
-    a, b, c, d = split(matrixA)
-    e, f, g, h = split(matrixB) 
-  
+def strassen_multiplication(matrix_a, matrix_b): 
+    """Return the result of matrix_a * matrix_b, calculated using Strassen's algorithm."""
+    n = len(matrix_a)
+    if n == 1: 
+        return matrix_a * matrix_b
+    # Recursively split the matrices into quadrants until the base case is reached. 
+    a, b, c, d = split(matrix_a)
+    e, f, g, h = split(matrix_b) 
     # Seven recursive calls 
-    P = strassenMultiplication(a, f - h)   
-    Q = strassenMultiplication(a + b, h)         
-    R = strassenMultiplication(c + d, e)         
-    S = strassenMultiplication(d, g - e)
-    T = strassenMultiplication(a + d, e + h)         
-    U = strassenMultiplication(b - d, g + h)   
-    V = strassenMultiplication(a - c, e + f)   
-  
-    # Matrix C
-    c11 = T + S - Q + U   
-    c12 = P + Q            
-    c21 = R + S             
-    c22 = P + T - R - V   
-  
-    # This combines the four quadrants 
+    p = strassen_multiplication(a, f - h)   
+    q = strassen_multiplication(a + b, h)         
+    r = strassen_multiplication(c + d, e)         
+    s = strassen_multiplication(d, g - e)
+    t = strassen_multiplication(a + d, e + h)         
+    u = strassen_multiplication(b - d, g + h)   
+    v = strassen_multiplication(a - c, e + f)   
+    # Compute the four quadrants of the result matrix
+    c11 = t + s - q + u   
+    c12 = p + q            
+    c21 = r + s             
+    c22 = p + t - r - v   
+    # Combine the four quadrants 
     c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))  
-  
     return c
 
-def divideConquerMultiplication(a, b):
+def divide_conquer_multiplication(a, b):
+    """Return the result of a * b, calculated using the divide-and-conquer method."""
     n = len(a)
     if n == 1:
         return a * b
-    else:
-        a11 = a[:int(len(a) / 2), :int(len(a) / 2)]
-        a12 = a[:int(len(a) / 2), int(len(a) / 2):]
-        a21 = a[int(len(a) / 2):, :int(len(a) / 2)]
-        a22 = a[int(len(a) / 2):, int(len(a) / 2):]
-
-        b11 = b[:int(len(b) / 2), :int(len(b) / 2)]
-        b12 = b[:int(len(b) / 2), int(len(b) / 2):]
-        b21 = b[int(len(b) / 2):, :int(len(b) / 2)]
-        b22 = b[int(len(b) / 2):, int(len(b) / 2):]
-
-        c11 = divideConquerMultiplication(a11, b11) + divideConquerMultiplication(a12, b21)
-        c12 = divideConquerMultiplication(a11, b12) + divideConquerMultiplication(a12, b22)
-        c21 = divideConquerMultiplication(a21, b11) + divideConquerMultiplication(a22, b21)
-        c22 = divideConquerMultiplication(a21, b12) + divideConquerMultiplication(a22, b22)
-
-        result = np.zeros((n, n))
-        result[:int(len(result) / 2), :int(len(result) / 2)] = c11
-        result[:int(len(result) / 2), int(len(result) / 2):] = c12
-        result[int(len(result) / 2):, :int(len(result) / 2)] = c21
-        result[int(len(result) / 2):, int(len(result) / 2):] = c22
-    return result
+    a11 = a[:n//2, :n//2]
+    a12 = a[:n//2, n//2:]
+    a21 = a[n//2:, :n//2]
+    a22 = a[n//2:, n//2:]
+    b11 = b[:n//2, :n//2]
+    b12 = b[:n//2, n//2:]
+    b21 = b[n//2:, :n//2]
+    b22 = b[n//2:, n//2:]
+    c11 = divide_conquer_multiplication(a11, b11) + divide_conquer_multiplication(a12, b21)
+    c12 = divide_conquer_multiplication(a11, b12) + divide_conquer_multiplication(a12, b22)
+    c21 = divide_conquer_multiplication(a21, b11) + divide_conquer_multiplication(a22, b21)
+    c22 = divide_conquer_multiplication(a21, b12) + divide_conquer_multiplication(a22, b22)
+    c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+    return c
 
 def main(): 
     # n is both dimensions of each 2D array 
@@ -94,8 +80,8 @@ def main():
     # while n <= 64:
     for x in range(0, 200):
         # This generates two randomly populated matrices.
-        MatrixA = matrixMultiplier(n)
-        MatrixB = matrixMultiplier(n)
+        MatrixA = matrix_multiplier(n)
+        MatrixB = matrix_multiplier(n)
         for y in range(0, 10):
        
             # This generates two randomly populated matrices.
@@ -104,7 +90,7 @@ def main():
         
             # This multiplies the matrices and times the operation.
             startTime = time.perf_counter_ns()
-            classicalMultiplication(MatrixA, MatrixB) 
+            classical_multiplication(MatrixA, MatrixB) 
             finishTime = time.perf_counter_ns()
         
             # Calculates the elapsed time in seconds.
@@ -112,7 +98,7 @@ def main():
             
             # This multiplies the matrices and times the operation.
             startTime = time.perf_counter_ns()
-            strassenMultiplication(MatrixA, MatrixB) 
+            strassen_multiplication(MatrixA, MatrixB) 
             finishTime = time.perf_counter_ns()
         
             # Calculates the elapsed time in seconds.
@@ -120,7 +106,7 @@ def main():
             
             # This multiplies the matrices and times the operation.
             startTime = time.perf_counter_ns()
-            divideConquerMultiplication(MatrixA, MatrixB) 
+            divide_conquer_multiplication(MatrixA, MatrixB) 
             finishTime = time.perf_counter_ns()
             
             # Calculates the elapsed time in seconds.
